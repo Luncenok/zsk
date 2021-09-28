fcc1acc936017140ea9c149f748e5d66e878245d
Mateusz Idziejczak <mateusz.idziejczak@uczen.zsk.poznan.pl>
2021-09-28 12:00:11 +0200

    Create form.php

---
+++ form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Figury geometryczne</title>
+  </head>
+  <body>
+    <h3>Figury geometryczne</h3>
+    <form action="./scripts/script.php" method="post">
+      <input type="text" name="name" placeholder="Podaj imię: "><br><br>
+      <input type="radio" name="geometrucFigure" value="kwadrat" >Kwadrat<br><br>
+      <input type="radio" name="geometrucFigure" value="prostokat">Prostokąt<br><br>
+      <input type="submit" value="Zatwierdź figure">
+    </form>
+  </body>
+</html>
