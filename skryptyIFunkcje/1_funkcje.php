b86b8896ca7bb42dfce119d9399291a6af78e9ef
Mateusz Idziejczak <mateusz.idziejczak@uczen.zsk.poznan.pl>
2021-10-19 11:37:39 +0200

    Create 1_funkcje.php

---
+++ skryptyIFunkcje/1_funkcje.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="./skrypty/1_skrypt.php" method="post">
+      <input type="text" name="name" placeholder="Podaj imie">
+      <input type="submit"  value="Wyślij">
+    </form>
+  </body>
+</html>
